1ac2b99a6d0d52fe3a903671724793685c6beba0
tepbuphata.kim <tepbuphata.kim@axivit.com>
2023-08-14 16:23:09 +0700

    Can delete now dude?

--- src/stages/profile.js
+++ src/stages/profile.js
@@ -25,12 +25,19 @@ profileScene.hears('💁 My profile', async (ctx) => {
       {
         caption: `${pf_name} | ${pf_description}`,
         parse_mode: 'Markdown',
+        ...Markup.inlineKeyboard([
+          [Markup.button.callback('Delete profile', '/delete-profile')],
+        ]),
       }
     );
   } catch (error) {
     console.error(error);
     return ctx.scene.enter('profile-scene');
   }
+});
+profileScene.action('/delete-profile', (ctx) => {
+  console.log('ctx: ', ctx.from.id);
+  console.log('Can delete now dude?');
 });
 profileScene.hears('👷 Setup profile', (ctx) => {
   ctx.scene.enter('setup-profile-wizard');

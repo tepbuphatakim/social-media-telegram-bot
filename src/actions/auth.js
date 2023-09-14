ba838e58df8863d6238c422f9df3c8d4f3a894fa
tepbuphata.kim <tepbuphata.kim@axivit.com>
2023-09-14 17:06:20 +0700

    Authentication with middleware

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,18 @@
 import jwt from 'jsonwebtoken';
+import Admin from '../models/Admin.js';
 
 const { JWT_SECRET_KEY } = process.env;
 
 export async function login(req, res, next) {
   try {
-    const token = jwt.sign({ id_user: 1 }, JWT_SECRET_KEY);
+    const { email, password } = req.body;
+    const admin = await Admin.findOne({
+      where: {
+        email,
+        password,
+      },
+    });
+    const token = jwt.sign({ admin }, JWT_SECRET_KEY, { expiresIn: '24h' });
     res.send(token);
   } catch (error) {
     next(error);
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import userRoute from './user.js';
 import feedRoute from './feed.js';
 import friendRoute from './friend.js';
+import { authentication } from '../middlewares/auth.js';
 
 const app = express();
 const port = 3000;
+
+app.use(authentication);
 
 app.use('/users', userRoute);
 app.use('/feeds', feedRoute);
